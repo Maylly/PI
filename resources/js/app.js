fe6bd61677211f273fd83b30cd84edd5e4df51d5
MarceloGames000 <gamesmarcelo274@gmail.com>
2023-10-11 16:39:18 -0300

    klklklkk

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,9 @@ import './header'
 import imgLogo from '../img/logo.svg'
 document.querySelectorAll('.logo-img').forEach((element) => {
     element.src = imgLogo;
-})
+});
 
 import imgUrl from '../img/icons/loupe.svg'
 document.querySelectorAll('.lupa-img').forEach((element) => {
     element.src = imgUrl;
-})
+});
